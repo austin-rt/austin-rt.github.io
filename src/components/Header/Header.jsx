99be231647d26f64f8d2762e223a2bd065728c36
austin-rt <austintaylorart@gmail.com>
2022-08-22 13:13:59 -0400

    removing unused code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,14 +5,15 @@ import CallToAction from './CallToAction';
 import HeaderSocials from './HeaderSocials';
 
 export default function Header() {
-  const strings = [
-    "and I'm ",
-    "a Full-Stack Software Developer.",
-    "Tinkerer.",
-    "Fixie Rider.",
-    "Film Photographer.",
-    "an Enneagram Five.",
-    "me!"];
+
+  // const strings = [
+  //   "and I'm ",
+  //   "a Full-Stack Software Developer.",
+  //   "Tinkerer.",
+  //   "Fixie Rider.",
+  //   "Film Photographer.",
+  //   "an Enneagram Five.",
+  //   "me!"];
 
 
 
