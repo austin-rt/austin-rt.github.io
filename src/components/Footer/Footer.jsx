4adbb7c75f57c9d20eea345c1c4da8ef172fa85b
austin-rt <austintaylorart@gmail.com>
2022-09-02 14:09:29 -0500

    add portfolio repo, restyle footer link color

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,34 +1,114 @@
-import React from 'react';
-import './Footer.css';
-import { BsLinkedin } from 'react-icons/bs';
-import { BsGithub } from 'react-icons/bs';
+import React from 'react'
+import './Footer.css'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
 
-export default function Footer({home, about, portfolio, experience, contact, scrollTo}) {
+export default function Footer({
+  home,
+  about,
+  portfolio,
+  experience,
+  contact,
+  scrollTo
+}) {
   return (
     <footer>
-      <h4 onClick={()=>{scrollTo(home)}} className='footer__logo'>austinrt.io</h4>
+      <h4
+        onClick={() => {
+          scrollTo(home)
+        }}
+        className="footer__logo"
+      >
+        austinrt.io
+      </h4>
 
-      <ul className='permalinks'>
-        <li onClick={()=>{scrollTo(home)}} >Home</li>
-        <li onClick={()=>{scrollTo(about)}}>About Me</li>
-        <li onClick={()=>{scrollTo(portfolio)}}>My Portfolio</li>
-        <li onClick={()=>{scrollTo(experience)}}>My Experience</li>
-        <li onClick={()=>{scrollTo(contact)}}>Contact Me</li>
+      <ul className="permalinks">
+        <li
+          onClick={() => {
+            scrollTo(home)
+          }}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => {
+            scrollTo(about)
+          }}
+        >
+          About Me
+        </li>
+        <li
+          onClick={() => {
+            scrollTo(portfolio)
+          }}
+        >
+          My Portfolio
+        </li>
+        <li
+          onClick={() => {
+            scrollTo(experience)
+          }}
+        >
+          My Experience
+        </li>
+        <li
+          onClick={() => {
+            scrollTo(contact)
+          }}
+        >
+          Contact Me
+        </li>
       </ul>
 
       <div className="footer__socials">
-        <a href="https://www.linkedin.com/in/austinrt" title="LinkedIn Profile" target="_blank" className='icon' rel='noopener noreferrer'><BsLinkedin /></a>
-        <a href="https://www.github.com/austin-rt" title="GitHub Page" target="_blank" className='icon' rel='noopener noreferrer'><BsGithub /></a>
+        <a
+          href="https://www.linkedin.com/in/austinrt"
+          title="LinkedIn Profile"
+          target="_blank"
+          className="icon"
+          rel="noopener noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://www.github.com/austin-rt"
+          title="GitHub Page"
+          target="_blank"
+          className="icon"
+          rel="noopener noreferrer"
+        >
+          <BsGithub />
+        </a>
       </div>
 
       <div className="footer__copyright">
         <div>
-          <small>&copy; Austin Taylor 2022.</small>
+          <small>&copy; Austin Taylor 2022.</small>{' '}
+          <small>
+            <a
+              href="https://github.com/austin-rt/austin-rt.github.io"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Portfolio Repo
+            </a>
+          </small>
         </div>
         <div>
-          <small>Page design based on Egator's Example <a href="https://egattor.com/rp2447/" target="_blank" rel='noopener noreferrer'> React Portfolio</a></small>
+          <small>
+            Page design based on Egator's Example{' '}
+            <a
+              href="https://egattor.com/rp2447/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="credit"
+            >
+              {' '}
+              React Portfolio
+            </a>
+          </small>
         </div>
       </div>
     </footer>
-  );
+  )
 }
