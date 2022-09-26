1803485d8de1527d162811a9299afa41e176ce53
austin-rt <austintaylorart@gmail.com>
2022-09-26 19:53:12 -0400

    updated resume

--- src/components/Header/CallToAction.jsx
+++ src/components/Header/CallToAction.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import Resume from '../../assets/Austin-Taylor-ATS-Resume.pdf';
+import React from 'react'
+import Resume from '../../assets/Austin-Taylor-ATS-Resume_2022-10.pdf'
 
-export default function CallToAction({contact, scrollTo}) {
+export default function CallToAction({ contact, scrollTo }) {
   return (
-    <div className='call-to-action'>
-      <a href={Resume} download className='btn'>Download Resume</a>
-      <div onClick={()=>{scrollTo(contact)}} className='btn btn-primary'>Let's Chat</div>
+    <div className="call-to-action">
+      <a href={Resume} download className="btn">
+        Download Resume
+      </a>
+      <div
+        onClick={() => {
+          scrollTo(contact)
+        }}
+        className="btn btn-primary"
+      >
+        Let's Chat
+      </div>
     </div>
-  );
+  )
 }
